d2320a8bca68182b50b4ab6430e015709dfd09e7
kamo16-cmd <kgomotsotlhasi16@gmail.com>
2023-07-23 14:31:04 -0700

    updates

--- root/printf/_printf.c
+++ root/printf/_printf.c
@@ -51,16 +51,3 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
-
-/**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char whichrepresents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
